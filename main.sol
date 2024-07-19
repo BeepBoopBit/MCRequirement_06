0da1cbc82b85b0770957554d1b89813107a3a668
Renz Aguirre <90445232+BeepBoopBit@users.noreply.github.com>
2024-07-19 13:17:02 +0800

    Add files via upload

---
+++ main.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Ownable {
+    struct Item {
+        string name;
+        uint256 price;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+    uint256 public nextItemId;
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) payable {}
+
+    function mint(address to, uint256 amount) public onlyOwner {    
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function addItem(string memory name, uint256 price, uint256 stock) public onlyOwner {
+        items[nextItemId] = Item(name, price, stock);
+        nextItemId++;
+    }
+
+    function redeem(uint256 itemId, uint256 amount) public {
+        require(items[itemId].stock >= amount, "Not enough items in stock");
+        uint256 cost = items[itemId].price * amount;
+        require(balanceOf(msg.sender) >= cost, "Not enough tokens");
+
+        _burn(msg.sender, cost);
+        items[itemId].stock -= amount;
+    }
+
+    function getItem(uint256 itemId) public view returns (string memory name, uint256 price, uint256 stock) {
+        Item storage item = items[itemId];
+        return (item.name, item.price, item.stock);
+    }
+}
